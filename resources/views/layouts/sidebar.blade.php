17dd3e8c83ebbedda54e62c8d50ce26da80d1268
RY1997 <riskydharmayudistira@gmail.com>
2023-11-03 00:31:00 +0900

    logo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ url('/home') }}" class="brand-link">
-        <img src="https://scontent.fcgk18-1.fna.fbcdn.net/v/t39.30808-6/295786293_410663044427529_3719825953568938092_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFu4DfvJzImpoDKQq3VXptqrt42s-uI27Su3jaz64jbtOrffqYe9kfrxls6FqrI0Lri3i0uWVRMkwCYQPs2UeOc&_nc_ohc=llPxJXm8oUUAX_5irpg&_nc_ht=scontent.fcgk18-1.fna&oh=00_AfAFM8FKYs0KHVI1RqiOA4AC_qSWPNUwrAfxUBQ0Uz9WHg&oe=653F0ABB"
+        <img src="https://upload.wikimedia.org/wikipedia/commons/1/11/BPKP_Logo.png"
              alt="Logo"
              class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">SiAPP BANSOS</span>
