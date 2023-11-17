806dbd902bfc0ed001e7980ab3de08a014aa7303
RY1997 <riskydharmayudistira@gmail.com>
2023-11-17 10:32:34 +0700

    edit tabel

--- resources/views/validasi_pemdas/table.blade.php
+++ resources/views/validasi_pemdas/table.blade.php
@@ -25,7 +25,7 @@
             <td>@rupiah($validasiPemda->r_5_1_06)</td>
             <td>@rupiah($rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->count() > 0 ? $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->sum('uji_anggaran') : 0)</td>
             <td>@rupiah($rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->count() > 0 ? $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->sum('uji_realisasi') : 0)</td>
-            <td><span class="badge text-xs {{ $validasiPemda->a_5_1_06 != $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->sum('uji_anggaran') || $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->sum('uji_anggaran') > 0 || $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->count() == 0 ? 'bg-danger' : 'bg-primary'}}">@persen($validasiPemda->a_5_1_06 > 0 ? $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->sum('uji_anggaran') / $validasiPemda->a_5_1_06 * 100 : 0)%</span></td>
+            <td><span class="badge text-xs {{ $validasiPemda->a_5_1_06 != $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->sum('uji_anggaran') || $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->sum('uji_anggaran') == 0 || $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->count() == 0 ? 'bg-danger' : 'bg-primary'}}">@persen($validasiPemda->a_5_1_06 > 0 ? $rincianBansos->where('nm_pemda' , $validasiPemda->nm_pemda)->sum('uji_anggaran') / $validasiPemda->a_5_1_06 * 100 : 0)%</span></td>
                 <td width="250">
                     <div class='btn-group'>
                         <a href="{{ route('validasiPemdas.show', [$validasiPemda->id]) }}"
