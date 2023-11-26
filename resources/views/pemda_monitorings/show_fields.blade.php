ab315b16f210b2f7d77a8b65a4376a6e0bf9d8a2
RY1997 <riskydharmayudistira@gmail.com>
2023-11-26 11:20:42 +0700

    ganti kata informasi belanja menjadi postur apbd

--- resources/views/pemda_monitorings/show_fields.blade.php
+++ resources/views/pemda_monitorings/show_fields.blade.php
@@ -23,7 +23,7 @@
 </div>
 
 <div class="form-group col-sm-12">
-    <h6>Informasi Belanja Daerah</h6>
+    <h6>Postur APBD</h6>
 </div>
 
 <table class="table table-bordered text-center" id="pemdaMonitorings-table">
