a1b9219c480c8ad1c5f36357b7df13ccaa4c8698
RY1997 <riskydharmayudistira@gmail.com>
2023-11-29 17:58:39 +0700

    tes tab

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -132,7 +132,7 @@
                     </li>
                 </ul>
                 <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">a</div>
+                    <div class="tab-pane fade active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">a</div>
                     <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">b</div>
                     <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">c</div>
                 </div>
