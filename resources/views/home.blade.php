6323d2a7607ad20bfe61363356e10f9938efff8b
RY1997 <riskydharmayudistira@gmail.com>
2023-11-29 17:59:25 +0700

    undo tabs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -120,22 +120,6 @@
         <div class="card">
             <div class="card-body p-3 text-center text-xs">
                 <h5>Burden Sharing Bantuan Sosial Daerah</h5>
-                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="tab" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Total</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="tab" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Bansos Tunai</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-contact-tab" data-bs-toggle="tab" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Bansos Barang</button>
-                    </li>
-                </ul>
-                <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">a</div>
-                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">b</div>
-                    <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">c</div>
-                </div>
                 <table class="table table-responsive table-bordered">
                     <thead>
                         <tr class="bg-primary">
